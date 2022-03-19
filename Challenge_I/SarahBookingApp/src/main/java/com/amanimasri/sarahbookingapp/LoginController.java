9f09c41044e6587522a430d8800707b05e198541
Mohamad <amanimasri92@gmail.com>
2022-03-19 21:01:16 +0200

    Updated LoginController and  SignUpController

--- Challenge_I/SarahBookingApp/src/main/java/com/amanimasri/sarahbookingapp/LoginController.java
+++ Challenge_I/SarahBookingApp/src/main/java/com/amanimasri/sarahbookingapp/LoginController.java
@@ -1,4 +1,26 @@
 package com.amanimasri.sarahbookingapp;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class LoginController {
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    void login(MouseEvent event) {
+
+    }
+
+    @FXML
+    void signup(MouseEvent event) {
+
+    }
+
 }
--- Challenge_I/SarahBookingApp/src/main/java/com/amanimasri/sarahbookingapp/SignUpController.java
+++ Challenge_I/SarahBookingApp/src/main/java/com/amanimasri/sarahbookingapp/SignUpController.java
@@ -1,4 +1,46 @@
 package com.amanimasri.sarahbookingapp;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class SignUpController {
+
+    @FXML
+    private TextField first_name;
+
+    @FXML
+    private TextField last_name;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private TextField email;
+
+
+    @FXML
+    private PasswordField password;
+
+
+    @FXML
+    private PasswordField confirm_password;
+
+    @FXML
+    private ComboBox<?> gender;
+
+
+
+    @FXML
+    private TextField phone_number;
+
+
+
+    @FXML
+    void signup(MouseEvent event) {
+
+    }
+
 }
